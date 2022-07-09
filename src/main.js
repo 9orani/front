2f0f0a52c716e30f235ce1f849c9fa03c03b4043
Jun Seol <physiogel@pusan.ac.kr>
2022-07-09 09:47:10 +0900

    feat: 프론트 video 화면

---
+++ src/main.js
@@ -0,0 +1,99 @@
+import { VideoPlayer } from './js/VideoPlayer.js';
+
+let videoPlayer;
+let playButton;
+
+const showPlayButton = () => {
+    if (!document.getElementById('playBuuton')) {
+        let elementPlayButton = document.createElement('img');
+
+        elementPlayButton.id = 'playButton';
+        elementPlayButton.src = 'images/Play.png';
+        elementPlayButton.alt = 'Start Streaming';
+
+        playButton = document
+            .getElementById('player')
+            .appendChild(elementPlayButton);
+        playButton.addEventListener('click', onClickPlayButton);
+    }
+};
+
+const onClickPlayButton = () => {
+    playButton.style.display = 'none';
+
+    // add video player
+    const elementVideo = document.createElement('video');
+    elementVideo.id = 'Video';
+    elementVideo.style.touchAction = 'none';
+
+    const playerDiv = document.getElementById('player');
+    playerDiv.appendChild(elementVideo);
+
+    setUpVideoPlayer([elementVideo]).then((value) => (videoPlayer = value));
+
+    // add fullscreen button
+    const elementFullscreenButton = document.createElement('img');
+    elementFullscreenButton.id = 'fullscreenButton';
+    elementFullscreenButton.src = 'images/FullScreen.png';
+
+    playerDiv.appendChild(elementFullscreenButton);
+
+    elementFullscreenButton.addEventListener('click', function () {
+        if (!document.fullscreenElement || !document.webkitFullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(
+                    Element.ALLOW_KEYBOARD_INPUT
+                );
+            } else {
+                if (playerDiv.style.position == 'absolute') {
+                    playerDiv.style.position = 'relative';
+                } else {
+                    playerDiv.style.position = 'absolute';
+                }
+            }
+        }
+    });
+
+    const onFullScreenChange = () => {
+        if (document.webkitFullscreenElement || document.fullscreenElement) {
+            playerDiv.style.position = 'absolute';
+            elementFullscreenButton.style.display = 'none';
+        } else {
+            playerDiv.style.position = 'relative';
+            elementFullscreenButton.style.display = 'block';
+        }
+    };
+
+    document.addEventListener('webkitfullscreenchange', onFullScreenChange);
+    document.addEventListener('fullscreenchange', onFullScreenChange);
+};
+
+const setUpVideoPlayer = async (elements) => {
+    const videoPlayer = new VideoPlayer(elements);
+
+    await videoPlayer.setUpConnection();
+    videoPlayer.ondisconnect = onDisconnect;
+
+    return videoPlayer;
+};
+
+const onDisconnect = (message) => {
+    if (message) {
+        messageDiv.style.display = 'block';
+        messageDiv.innerText = message;
+    }
+
+    clearChildren(playerDiv);
+    videoPlayer = null;
+    showPlayButton();
+};
+
+const clearChildren = (element) => {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+};
+
+showPlayButton();
