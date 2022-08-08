3c6a7d88fd93fe459782c1c4ad829a893f45e0b6
redundant4u <physiogel@pusan.ac.kr>
2022-08-09 00:52:14 +0900

    feat: nginx를 통해 spring boot 통신하도록 설정

--- docker/configs/entrypoint.sh
+++ docker/configs/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sed -i "s/WEBRTC_ADDRESS/${WEBRTC_ADDRESS}/" /etc/nginx/conf.d/default.conf
+sed -i "s/SPRING_ADDRESS/${SPRING_ADDRESS}/" /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
