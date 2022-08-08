92526568bb544ab11512bebdc88997a0cf5db822
redundant4u <physiogel@pusan.ac.kr>
2022-08-09 01:17:49 +0900

    refactor: turn 서버 설정 추가

--- src/js/config.js
+++ src/js/config.js
@@ -9,7 +9,14 @@ const getRTCConfiguration = () => {
     let config = {};
 
     config.sdpSemantics = 'unified-plan';
-    config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+    config.iceServers = [
+        { urls: ['stun:stun.l.google.com:19302'] },
+        {
+            urls: ['turn:27.96.131.42:3478?transport=tcp'], 
+            username: 'gorani', 
+            credential: 'gorani'
+        }
+    ];
 
     return config;
 };
